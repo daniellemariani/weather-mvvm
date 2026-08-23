c7a07ddcaffc880d2e484f9aeb2df68eeff03f06
Danielle Mariani <danielle.mariani@gmail.com>
2026-08-22 17:22:40 -0700

    Add NetworkModule

---
+++ app/src/main/java/com/dmariani/weathermvvm/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.dmariani.weathermvvm.di
+
+import com.dmariani.weathermvvm.data.remote.WeatherApi
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.open-meteo.com/"
+
+    @Provides @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+
+    @Provides @Singleton
+    fun provideWeatherApi(retrofit: Retrofit): WeatherApi {
+        return retrofit.create(WeatherApi::class.java)
+    }
+}
