c53326038ade16bb6bc85663e42f220ef4c6a9f1
Danielle Mariani <danielle.mariani@gmail.com>
2026-08-23 22:21:50 -0700

    Add WeatherViewModel

---
+++ app/src/main/java/com/dmariani/weathermvvm/ui/main/WeatherViewModel.kt
@@ -0,0 +1,89 @@
+package com.dmariani.weathermvvm.ui.main
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dmariani.weathermvvm.domain.model.City
+import com.dmariani.weathermvvm.domain.model.Weather
+import com.dmariani.weathermvvm.domain.repository.WeatherRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class WeatherViewModel @Inject constructor(private val repository: WeatherRepository) :
+    ViewModel() {
+
+    private var lastCity: City? = null
+
+    private val _weather = MutableLiveData<Weather>()
+    val weather: LiveData<Weather> get() = _weather
+
+    private val _isLoading = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: LiveData<String?> get() = _errorMessage
+
+    private val _recentSearches = MutableLiveData<List<String>>()
+    val recentSearches: LiveData<List<String>> get() = _recentSearches
+
+    private val _snackbarEvent = MutableLiveData<Event<String>>()
+    val snackbarEvent: LiveData<Event<String>> get() = _snackbarEvent
+
+    private val disposables = CompositeDisposable()
+
+    init {
+        disposables.add(
+            repository.observeRecentSearches()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { list -> _recentSearches.value = list },
+                    { error ->
+                        Log.e(
+                            "WeatherViewModel",
+                            "Failed to observe recent searches",
+                            error
+                        )
+                    }
+                )
+        )
+    }
+
+    fun onCitySelected(city: City) {
+        lastCity = city
+        fetchWeather()
+    }
+
+    fun onRetry() {
+        fetchWeather(true)
+    }
+
+    private fun fetchWeather(forceRefresh: Boolean = false) {
+        val city = lastCity ?: return
+
+        _isLoading.value = true
+
+        disposables.add(
+            repository.getWeather(city, forceRefresh)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doFinally { _isLoading.value = false }
+                .subscribe(
+                    { weather -> /* do something */
+                        _weather.value = weather
+                        _errorMessage.value = null
+                    },
+                    { error ->
+                        val errorCopy = "Unable to load weather from ${city.name}"
+                        Log.e("WeatherViewModel", errorCopy, error)
+                        _errorMessage.value = errorCopy
+                        _snackbarEvent.value = Event(errorCopy)
+                    }
+                )
+        )
+    }
+
+    override fun onCleared() {
+        disposables.dispose()
+    }
+}
